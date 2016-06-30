8fffdee3581116964b5f4d272315a14ffba05a2b
Kurt Aquilina <kurt.aquilina@betssongroup.com>
2016-06-30 08:36:20 +0200

    removed typings from being global

--- fuse.js.d.ts
+++ fuse.js.d.ts
@@ -1,28 +1,25 @@
-declare namespace fuse {
-	interface IFuseOptions {
-		id?: string;
-		caseSensitive?: boolean;
-		include?: string[];
-		shouldSort?: boolean;
-		searchFn?: any;
-		sortFn?: (a: { score: number }, b: { score: number }) => number;
-		getFn?: (obj: any, path: string) => any;
-		keys?: string[] | { name: string; weight: number }[];
-		verbose?: boolean;
-		tokenize?: boolean;
-		location?: number;
-		distance?: number;
-		threshold?: number;
-		maxPatternLength?: number;
-	}
-
-	interface Fuse {
-		new (list: any[], options?: IFuseOptions): Fuse;
-		search<T>(pattern: string): T[];
-	}
+declare class Fuse {
+    constructor (list: any[], options?: Fuse.IFuseOptions);
+    search<T>(pattern: string): T[];
 }
 
-declare module "fuse" {
-	var fuse: fuse.Fuse;
-	export = fuse;
+declare namespace Fuse {
+    export interface IFuseOptions {
+        id?: string;
+        caseSensitive?: boolean;
+        include?: string[];
+        shouldSort?: boolean;
+        searchFn?: any;
+        sortFn?: (a: { score: number }, b: { score: number }) => number;
+        getFn?: (obj: any, path: string) => any;
+        keys?: string[] | { name: string; weight: number }[];
+        verbose?: boolean;
+        tokenize?: boolean;
+        location?: number;
+        distance?: number;
+        threshold?: number;
+        maxPatternLength?: number;
+    }
 }
+
+export = Fuse;
