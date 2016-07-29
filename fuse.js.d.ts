c5175d339b9fe0d7eab52c3133480c4131f758c9
Oto Brglez <otobrglez@gmail.com>
2016-07-29 02:47:36 +0200

    This work for me.

--- fuse.js.d.ts
+++ fuse.js.d.ts
@@ -1,25 +1,29 @@
-declare class Fuse {
-    constructor (list: any[], options?: Fuse.IFuseOptions);
-    search<T>(pattern: string): T[];
-}
+declare module "fuse.js" {
 
-declare namespace Fuse {
-    export interface IFuseOptions {
-        id?: string;
-        caseSensitive?: boolean;
-        include?: string[];
-        shouldSort?: boolean;
-        searchFn?: any;
-        sortFn?: (a: { score: number }, b: { score: number }) => number;
-        getFn?: (obj: any, path: string) => any;
-        keys?: string[] | { name: string; weight: number }[];
-        verbose?: boolean;
-        tokenize?: boolean;
-        location?: number;
-        distance?: number;
-        threshold?: number;
-        maxPatternLength?: number;
+    class Fuse {
+        constructor(list:any[], options?:Fuse.IFuseOptions);
+
+        search<T>(pattern:string):T[];
     }
-}
 
-export = Fuse;
+    namespace Fuse {
+        export interface IFuseOptions {
+            id?:string;
+            caseSensitive?:boolean;
+            include?:string[];
+            shouldSort?:boolean;
+            searchFn?:any;
+            sortFn?:(a:{ score:number }, b:{ score:number }) => number;
+            getFn?:(obj:any, path:string) => any;
+            keys?:string[] | { name:string; weight:number }[];
+            verbose?:boolean;
+            tokenize?:boolean;
+            location?:number;
+            distance?:number;
+            threshold?:number;
+            maxPatternLength?:number;
+        }
+    }
+
+    export = Fuse;
+}
